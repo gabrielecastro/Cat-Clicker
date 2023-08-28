54f7f1aa46222e1a2ba93e284499d86a613a8857
Gabriele Castro <gabriele.castro@amicci.com.br>
2023-08-28 17:24:13 -0300

    segunda mudança de requisitos

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
-// inicia contador
-let contadorGarfield = 0;
-let contadorTobias = 0;
+// inicia os contadores
+const counters = {};
 
-// pega a imagem e contador
-const imageGarfield = document.getElementById('garfield');
-const imageTobias = document.getElementById('tobias');
-const counterGarfield = document.getElementById('counter-garfield');
-const counterTobias = document.getElementById('counter-tobias');
+// array com todos os gatos
+const allCats = [
+  { img: 'assets/cat1.jpg', name: "Garfield"},
+  { img: 'assets/cat2.jpg', name: "Tadeu"},
+  { img: 'assets/cat3.jpg', name: "José"},
+  { img: 'assets/cat4.jpg', name: "Marie"},
+  { img: 'assets/cat5.jpg', name: "Joãozinho"},
+];
 
-// função chamada quando a imgem é clicada
+const createHtmlImages = () => {
+  const container = document.getElementById('all-cats');
+
+  const htmlImages = allCats.map(cat => {
+    return `<p class="name-all-cats" id=${cat.name}>${cat.name} </>`;
+  });
+
+  container.innerHTML = htmlImages.join('');
+
+  // Adicione um ouvinte de evento de clique a cada nome de gato
+  allCats.forEach(cat => {
+    const catElement = document.getElementById(cat.name);
+    catElement.addEventListener('click', () => selectCat(cat));
+  });
+};
+
+const selectCat = (cat) => {
+  const selectedCatDiv = document.querySelector('.selected-cat');
+  selectedCatDiv.innerHTML = `
+    <h2>${cat.name}</h2>
+    <img src="${cat.img}" alt="${cat.name}">
+    <p class="counter" id="counter-${cat.name}">0</p>
+  `;
+
+  const counterElement = document.getElementById(`counter-${cat.name}`);
+  counterElement.textContent = counters[cat.name] || 0;
+
+  const catImage = selectedCatDiv.querySelector('img');
+  catImage.addEventListener('click', () => increaseCounter(cat));
+};
+
 const increaseCounter = (cat) => {
-  if (cat === "Garfield") {
-    contadorGarfield += 1;
-    counterGarfield.textContent = contadorGarfield;
-  }
-  if (cat === "Tobias") {
-    contadorTobias += 1;
-    counterTobias.textContent = contadorTobias;
-  }
-  
+  counters[cat.name] = (counters[cat.name] || 0) + 1;
+  const counterElement = document.getElementById(`counter-${cat.name}`);
+  counterElement.textContent = counters[cat.name];
 }
 
-// Adiciona um ouvinte de evento de clique à imagem
-imageGarfield.addEventListener('click', () => increaseCounter('Garfield'));
-imageTobias.addEventListener('click', () => increaseCounter('Tobias'));
+// Chame a função para criar as imagens quando a página carregar
+window.onload = createHtmlImages;
