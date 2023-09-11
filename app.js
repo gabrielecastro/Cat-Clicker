6752137517647488e50183d57201ba0b703362d7
Gabriele Castro <gabriele.castro@amicci.com.br>
2023-09-11 16:31:20 -0300

    refatora o código pra dividir as responsabilidades

--- app.js
+++ app.js
@@ -1,51 +1,68 @@
-// inicia os contadores
-const counters = {};
-
-// array com todos os gatos
-const allCats = [
-  { img: 'assets/cat1.jpg', name: "Garfield"},
-  { img: 'assets/cat2.jpg', name: "Tadeu"},
-  { img: 'assets/cat3.jpg', name: "José"},
-  { img: 'assets/cat4.jpg', name: "Marie"},
-  { img: 'assets/cat5.jpg', name: "Joãozinho"},
-];
-
-const createHtmlImages = () => {
-  const container = document.getElementById('all-cats');
-
-  const htmlImages = allCats.map(cat => {
-    return `<p class="name-all-cats" id=${cat.name}>${cat.name} </>`;
-  });
-
-  container.innerHTML = htmlImages.join('');
-
-  // Adicione um ouvinte de evento de clique a cada nome de gato
-  allCats.forEach(cat => {
-    const catElement = document.getElementById(cat.name);
-    catElement.addEventListener('click', () => selectCat(cat));
-  });
+// Model
+const model = {
+  counters: {},
+  allCats: [
+    { img: 'assets/cat1.jpg', name: "Garfield" },
+    { img: 'assets/cat2.jpg', name: "Tadeu" },
+    { img: 'assets/cat3.jpg', name: "José" },
+    { img: 'assets/cat4.jpg', name: "Marie" },
+    { img: 'assets/cat5.jpg', name: "Joãozinho" },
+  ],
 };
 
-const selectCat = (cat) => {
-  const selectedCatDiv = document.querySelector('.selected-cat');
-  selectedCatDiv.innerHTML = `
-    <h2>${cat.name}</h2>
-    <img src="${cat.img}" alt="${cat.name}">
-    <p class="counter" id="counter-${cat.name}">0</p>
-  `;
-
-  const counterElement = document.getElementById(`counter-${cat.name}`);
-  counterElement.textContent = counters[cat.name] || 0;
-
-  const catImage = selectedCatDiv.querySelector('img');
-  catImage.addEventListener('click', () => increaseCounter(cat));
+// Octopus (Controller)
+const octopus = {
+  init: function () {
+    view.init();
+  },
+  getAllCats: function () {
+    return model.allCats;
+  },
+  getCounter: function (catName) {
+    return model.counters[catName] || 0;
+  },
+  increaseCounter: function (catName) {
+    model.counters[catName] = (model.counters[catName] || 0) + 1;
+    view.renderSelectedCat(catName);
+  },
 };
 
-const increaseCounter = (cat) => {
-  counters[cat.name] = (counters[cat.name] || 0) + 1;
-  const counterElement = document.getElementById(`counter-${cat.name}`);
-  counterElement.textContent = counters[cat.name];
-}
+// View
+const view = {
+  init: function () {
+    this.container = document.getElementById('all-cats');
+    this.selectedCatDiv = document.querySelector('.selected-cat');
 
-// Chame a função para criar as imagens quando a página carregar
-window.onload = createHtmlImages;
+    this.createHtmlImages();
+
+    octopus.init();
+  },
+  createHtmlImages: function () {
+    const htmlImages = octopus.getAllCats().map(cat => {
+      return `<p class="name-all-cats" id=${cat.name}>${cat.name} </>`;
+    });
+
+    this.container.innerHTML = htmlImages.join('');
+
+    octopus.getAllCats().forEach(cat => {
+      const catElement = document.getElementById(cat.name);
+      catElement.addEventListener('click', () => this.renderSelectedCat(cat.name));
+    });
+  },
+  renderSelectedCat: function (catName) {
+    const cat = octopus.getAllCats().find(cat => cat.name === catName);
+    this.selectedCatDiv.innerHTML = `
+      <h2>${cat.name}</h2>
+      <img src="${cat.img}" alt="${cat.name}">
+      <p class="counter" id="counter-${cat.name}">${octopus.getCounter(cat.name)}</p>
+    `;
+
+    const catImage = this.selectedCatDiv.querySelector('img');
+    catImage.addEventListener('click', () => octopus.increaseCounter(cat.name));
+  },
+};
+
+// Call the function to create the images when the page loads
+window.onload = function () {
+  octopus.init();
+};
